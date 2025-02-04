bb211d463ab81752587dc896ff4ef3f5b750b881
coldtema <coldtema1703@gmail.com>
2025-02-05 02:10:37 +0300

    added home endpoint (hello)

--- minishop/main.py
+++ minishop/main.py
@@ -5,8 +5,12 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/{name}")
-def top(name):
+def top_name(name):
     return {"name":name, "message": name[::-1]}
+
+@app.get("/")
+def name():
+    return "hello"
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
